fb8a0420f73cf505094de547bfcd6947a7ac7042
Anand.C <anandchannazhi01@gmail.com>
2024-02-09 11:02:49 +0530

    route added in __init__.py

--- shop/__init__.py
+++ shop/__init__.py
@@ -4,3 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////myshop.db'
 db = SQLAlchemy(app)
+
+#register the route here
+from shop import routes
