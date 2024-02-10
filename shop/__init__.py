e6eba88732f0ccb937867c119f35ec4224725ed1
Anand.C <anandchannazhi01@gmail.com>
2024-02-10 14:35:52 +0530

    SECRET_KEY and bcrypt

--- shop/__init__.py
+++ shop/__init__.py
@@ -4,7 +4,9 @@ from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///myshop.db'
+app.config['SECRET_KEY'] = 'jwkhfciuewhfwzf323f3'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 #  Import routes at the end to avoid circular imports
 from shop import routes
