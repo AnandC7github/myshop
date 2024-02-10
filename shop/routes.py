5ccfaf5f4b0faeb1a6b8f3c3fe0397d0b8bedc06
Anand.C <anandchannazhi01@gmail.com>
2024-02-10 15:31:11 +0530

    code for db was uncommented and a new import

--- shop/routes.py
+++ shop/routes.py
@@ -1,6 +1,7 @@
 # shop/routes.py
 from flask import render_template, session, request, redirect, url_for, flash
 from shop import app, db , bcrypt
+from shop.admin.models import User, db_session
 
 from shop.admin.forms import RegistrationForm
 import os
@@ -13,9 +14,10 @@ def home():
 def register():
     form = RegistrationForm(request.form)
     if request.method == 'POST' and form.validate():
-        # user = User(form.username.data, form.email.data,
-        #             form.password.data)
-        # db_session.add(user)
+        hash_password = bcrypt.generate_password_hash(form.password.data)
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db_session.add(user)
         flash('Thanks for registering')
         return redirect(url_for('login'))
     return render_template('admin/register.html', form=form, title='Registeration Page')
